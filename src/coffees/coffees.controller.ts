b6b643e6c31b40da76da11932892bf51345a2726
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-17 15:48:44 +0200

    Official NestJS Courses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
 @Controller("coffees")
 export class CoffeesController {
   // @Get("flavors")
   @Get()
   findAll() {
-    return "This action return all coffees";
+    return "This action returns all coffees";
+  }
+
+  @Get(":id")
+  // findOne(@Param() params) {
+  findOne(@Param("id") id: string) {
+    return `This action returns #${id} coffee`;
   }
 }
