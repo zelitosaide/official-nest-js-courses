b791eae425a8b60fb2e4d04717be9dc4c48bcdd0
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-17 17:44:05 +0200

    Official NestJS Courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [CoffeesModule],
-  controllers: [AppController, CoffeesController],
-  providers: [AppService, CoffeesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
