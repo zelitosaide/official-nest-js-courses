779350c972e490364fc3022c7dd1470f58b0b537
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-17 17:55:35 +0200

    Official NestJS Courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { CoffeesController } from "./coffees/coffees.controller";
-import { CoffeesService } from "./coffees/coffees.service";
+// import { CoffeesController } from "./coffees/coffees.controller";
+// import { CoffeesService } from "./coffees/coffees.service";
 import { CoffeesModule } from "./coffees/coffees.module";
 
 @Module({
   imports: [CoffeesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    // CoffeesController
+  ],
+  providers: [
+    AppService,
+    // CoffeesService
+  ],
 })
 export class AppModule {}
