446b490f8b66e3b42d2ac06d38abcb688a65f30c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-17 20:46:08 +0200

    Official NestJS Courses

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,49 +5,56 @@ import {
   NotFoundException,
 } from "@nestjs/common";
 import { Coffee } from "./entities/coffee.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: "Shipwreck Roast",
-      brand: "Buddy Brew",
-      flavors: ["chocolate", "vanilla"],
-    },
-  ];
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+  
+  // private coffees: Coffee[] = [
+  //   {
+  //     id: 1,
+  //     name: "Shipwreck Roast",
+  //     brand: "Buddy Brew",
+  //     flavors: ["chocolate", "vanilla"],
+  //   },
+  // ];
 
   findAll() {
-    return this.coffees;
+    // return this.coffees;
   }
 
   findOne(id: string) {
     // throw "A random error";
     // throw new Error();
-    const coffee = this.coffees.find((item) => item.id === +id);
-    if (!coffee) {
-      // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
-      throw new NotFoundException(`Coffee #${id} not found`);
-    }
-    return coffee;
+    // const coffee = this.coffees.find((item) => item.id === +id);
+    // if (!coffee) {
+    //   // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    //   throw new NotFoundException(`Coffee #${id} not found`);
+    // }
+    // return coffee;
   }
 
   create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
-    return createCoffeeDto;
+    // this.coffees.push(createCoffeeDto);
+    // return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id);
-    if (existingCoffee) {
-      // update the existing entity
-    }
+    // const existingCoffee = this.findOne(id);
+    // if (existingCoffee) {
+    //   // update the existing entity
+    // }
   }
 
   remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+    // const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    // if (coffeeIndex >= 0) {
+    //   this.coffees.splice(coffeeIndex, 1);
+    // }
   }
 }
