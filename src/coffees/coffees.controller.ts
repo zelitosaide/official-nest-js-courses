294904ca36747a793912ebad11145229a1fa6254
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-17 15:32:04 +0200

    Official NestJS Courses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from "@nestjs/common";
 
 @Controller("coffees")
 export class CoffeesController {
+  // @Get("flavors")
   @Get()
   findAll() {
     return "This action return all coffees";
