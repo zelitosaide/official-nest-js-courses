365a4e5c3f569d7869240ebe6fc11c7905bb7063
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-19 09:37:20 +0200

    Official NestJS Courses

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Injectable, Module } from "@nestjs/common";
 import { CoffeesController } from "./coffees.controller";
 import { CoffeesService } from "./coffees.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
@@ -12,6 +12,14 @@ import { COFFEE_BRANDS } from "./coffees.constants";
 // class ConfigService {}
 // class DevelopmentConfigService {}
 // class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // do something ...
+    return ["buddy brew", "nescafe"];
+  }
+}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
@@ -37,9 +45,13 @@ import { COFFEE_BRANDS } from "./coffees.constants";
     //       ? DevelopmentConfigService
     //       : ProductionConfigService,
     // },
+    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: () => ["buddy brew", "nescafe"],
+      // useFactory: () => ["buddy brew", "nescafe"],
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     },
   ],
   exports: [CoffeesService],
