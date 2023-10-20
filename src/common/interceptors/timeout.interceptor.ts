3ba3e3bdb751f4e042228c814efdbc33d8caa291
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 15:22:34 +0200

    Official NestJS Courses

--- src/common/interceptors/timeout.interceptor.ts
+++ src/common/interceptors/timeout.interceptor.ts
@@ -4,11 +4,11 @@ import {
   Injectable,
   NestInterceptor,
 } from "@nestjs/common";
-import { Observable } from "rxjs";
+import { Observable, timeout } from "rxjs";
 
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    return next.handle().pipe(timeout(3000));
   }
 }
