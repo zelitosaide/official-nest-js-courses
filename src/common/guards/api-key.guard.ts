06dbf151128688eb23472b569587dd88d60b5cab
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 13:55:58 +0200

    Official NestJS Courses

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,4 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Request } from "express";
 import { Observable } from "rxjs";
 
 @Injectable()
@@ -6,6 +7,7 @@ export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
     return false;
   }
 }
