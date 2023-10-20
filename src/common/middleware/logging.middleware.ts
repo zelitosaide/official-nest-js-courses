d5794ec479adc6ea5f0fa4b2eda269abe1d676db
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 16:21:10 +0200

    Official NestJS Courses

--- src/common/middleware/logging.middleware.ts
+++ src/common/middleware/logging.middleware.ts
@@ -3,7 +3,11 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    // console.log("Hi from middleware!");
+    console.time("Request-response time");
     console.log("Hi from middleware!");
+
+    res.on("finish", () => console.timeEnd("Request-response time"));
     next();
   }
 }
