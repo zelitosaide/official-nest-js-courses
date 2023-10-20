af67bf9de1bc9b68e2e32912dd26f3136ec00b60
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 16:18:35 +0200

    Official NestJS Courses

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -16,10 +16,11 @@ import { LoggingMiddleware } from "./middleware/logging.middleware";
 // export class CommonModule {}
 export class CommonModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(LoggingMiddleware).forRoutes("*");
+    consumer.apply(LoggingMiddleware).forRoutes("*");
     // consumer.apply(LoggingMiddleware).forRoutes("coffees");
-    consumer
-      .apply(LoggingMiddleware)
-      .forRoutes({ path: "coffees", method: RequestMethod.GET });
+    // consumer
+    //   .apply(LoggingMiddleware)
+    //   .forRoutes({ path: "coffees", method: RequestMethod.GET });
+    // consumer.apply(LoggingMiddleware).exclude("coffees").forRoutes("*");
   }
 }
