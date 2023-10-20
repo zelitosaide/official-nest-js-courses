b94024d2160114612e0de8815c3989684be6c4e0
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 15:39:30 +0200

    Official NestJS Courses

--- src/common/pipes/parse-int.pipe.spec.ts
+++ src/common/pipes/parse-int.pipe.spec.ts
@@ -1,7 +1,7 @@
-import { ParseIntPipe } from './parse-int.pipe';
+import { ParseIntPipe } from "./parse-int.pipe";
 
-describe('ParseIntPipe', () => {
-  it('should be defined', () => {
+describe("ParseIntPipe", () => {
+  it("should be defined", () => {
     expect(new ParseIntPipe()).toBeDefined();
   });
 });
