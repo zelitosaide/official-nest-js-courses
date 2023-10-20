546d93201930a368cf24726b85742ab173ae1f87
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 08:50:55 +0200

    Official NestJS Courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,20 @@ import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CoffeeRatingModule } from "./coffee-rating/coffee-rating.module";
+import { ConfigModule } from "@nestjs/config";
 // import { DatabaseModule } from "./database/database.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: "postgres", // type of our database
-      host: "localhost", // database host
-      port: 5433, // database host
-      username: "postgres", // username
-      password: "pass123", // user password
-      database: "postgres", // name of our database,
+      host: process.env.DATABASE_HOST, // database host
+      port: +process.env.DATABASE_PORT, // database host
+      username: process.env.DATABASE_USER, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
     }),
