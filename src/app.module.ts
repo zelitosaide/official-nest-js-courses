d52f8ae6b6bb8ebcdc6dba34dafcab640c597b65
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 10:00:53 +0200

    Official NestJS Courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,18 @@ import appConfig from "./config/app.config";
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: "postgres",
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     ConfigModule.forRoot({
       // envFilePath: ".environment",
       // envFilePath: ".env",
@@ -26,16 +38,16 @@ import appConfig from "./config/app.config";
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: "postgres", // type of our database
-      host: process.env.DATABASE_HOST, // database host
-      port: +process.env.DATABASE_PORT, // database host
-      username: process.env.DATABASE_USER, // username
-      password: process.env.DATABASE_PASSWORD, // user password
-      database: process.env.DATABASE_NAME, // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: "postgres", // type of our database
+    //   host: process.env.DATABASE_HOST, // database host
+    //   port: +process.env.DATABASE_PORT, // database host
+    //   username: process.env.DATABASE_USER, // username
+    //   password: process.env.DATABASE_PASSWORD, // user password
+    //   database: process.env.DATABASE_NAME, // name of our database,
+    //   autoLoadEntities: true, // models will be loaded automatically
+    //   synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    // }),
     CoffeeRatingModule,
     // DatabaseModule,
   ],
